b4fbca45018b322fbb4600ec71675a8f5cba8c8f
shivasupraj <shivasupraj.p@gmail.com>
2018-05-28 17:13:02 -0500

    Create 39. Combination Sum.py

---
+++ 39. Combination Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        def combinationSumHelper(candidates, target, val):
+            if target == 0:
+                res.append(val.copy())
+                return
+            if target < 0:
+                return
+            for i in range(len(candidates)):                
+                val.append(candidates[i])
+                combinationSumHelper(candidates[i:], target-candidates[i], val)
+                val.pop()
+        combinationSumHelper(candidates, target, [])
+        return res
+                
