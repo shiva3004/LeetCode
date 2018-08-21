f4e8cb4858ce4b69fa04c5677f586e78f17fc67a
shivasupraj <shivasupraj.p@gmail.com>
2018-08-21 10:43:44 -0500

    Create 32. Longest Valid Parentheses.py

---
+++ 32. Longest Valid Parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = [-1]
+        l_max = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) == 0:
+                    stack.append(i)
+                else:
+                    l = i - stack[-1]
+                    if l_max < l:
+                        l_max = l
+        return l_max
+            
